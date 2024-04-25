77b11603d7137c44643a16c87d0dc391389e11f3
akumar23 <aryan.kumar@nalej.com>
2024-04-24 22:58:22 -0700

    Updated home page added typewriter text, added dropdown menus for work and displayed projects

--- pages/index.js
+++ pages/index.js
@@ -13,21 +13,16 @@ import {
   chakra,
   Tab,
   Center,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel
 } from "@chakra-ui/react";
-import Paragraph from "../components/paragraph";
 import Section from "../components/section";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { IoLogoGithub } from "react-icons/io5";
-
-import { GridItem } from "../components/grid-item";
-import { motion } from "framer-motion";
-
-import thumbHal from "../public/projectImage/hal.png";
-import thumbFast from "../public/projectImage/fast-pages.png";
-import thumbSnake from "../public/projectImage/snake.png";
-import thumbRest from "../public/projectImage/rest.png";
-import thumbRank from "../public/projectImage/heroRank.png";
-import thumbTest from "../public/projectImage/ai-test.png";
+import Typewriter from 'typewriter-effect';
 
 const ProfileImage = chakra(Image, {
   shouldForwardProp: (prop) => ["width", "height", "src", "alt"].includes(prop),
@@ -38,10 +33,16 @@ const Home = () => (
     <br></br>
     <Box display={{ md: "flex" }}>
       <Box flexGrow={1}>
-        <Heading as="h2" variant="page-title">
-          Aryan Kumar
+        <Heading as='h2' size='lg' variant="page-title">
+          <Typewriter
+            options={{
+              strings: ['software engineer', 'full stack developer', 'ai engineer'],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </Heading>
-        <p>Software Engineer / Full Stack Developer</p>
+        <br></br>
         <p>B.S in Software Engineering from San Jose State University</p>
         <p>Masters in CS from Arizona State University (in progress)</p>
       </Box>
@@ -79,90 +80,134 @@ const Home = () => (
         Work
       </Heading>
 
-      <h4>
-        <b>Software Engineer @ NALEJ</b>
-      </h4>
+      <Accordion>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as='span' flex='1' textAlign='left'>
+                <b>Software Engineer @ NALEJ (Sep 2022-Present)</b>
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          
+          <AccordionPanel pb={4}>
+            <p>
+              designed, developed and deployed a webpage with next.js and typescript, tailwind and trpc 
+              that consumed the gitlab API to provide statistics for all projects that are in that gitlab 
+              instance
+            </p>
 
-      <Paragraph>
-        -designed, developed and deployed a webpage with next.js and typescript, tailwind and trpc that consumed
-        the gitlab API to provide statistics for all projects that are in that
-        gitlab instance
-        <br></br>
-        -took on a engineering lead role in hosting LLMs on kuberntes in an
-        air-gapped environment
-        <br></br>
-        -developed a discourse plugin for enhaned UX
-        <br></br>
-        -worked on adding capabilities to an opertaor to automate deployment of
-        services with desired settings
-        <br></br>
-        -containerized applications and services with docker for deployment in
-        kubernetes
-        </Paragraph>
+            <br></br>
+            
+            <p>
+              took on a engineering lead role in hosting LLMs on kuberntes in an
+              air-gapped environment
+            </p>
 
-        <br></br>
+            <br></br>
 
-        <h4>
-          <b>Freelance Python Developer @ Expert Witness Profiler LLC</b>
-        </h4>
+            <p>
+              developed a discourse plugin for enhaned UX
+            </p>
 
-        <Paragraph>
-        -designed a{" "}
-        <NextLink
-          href="https://github.com/akumar23/CourtScraper"
-          passHref
-          scroll={false}
-        >
-          <Link>python script</Link>
-        </NextLink>{" "}
-        that automated data collection from online courts using beautiful soup
-        and selenium and used pandas to make data frames for that webscraped
-        data
-        <br></br>
-        <br></br>
+            <br></br>
 
-        Some notable projects that I've worked on are:
-        <br></br>
-        <NextLink
-          href="https://snake-webapp.herokuapp.com/"
-          passHref
-          scroll={false}
-        >
-          <Link>Snake Game Webapp</Link>
-        </NextLink>
-        <br></br>
-        <NextLink
-          href="https://youtu.be/dVwozVz11ho?t=78"
-          passHref
-          scroll={false}
-        >
-          <Link>HAL: AI Chatbot Demo Video</Link>
-        </NextLink>
-        <br></br>
-        <NextLink
-          href="https://youtu.be/FfhoEv2LEoI?t=17"
-          passHref
-          scroll={false}
-        >
-          <Link>Fast Pages Demo Video</Link>
-        </NextLink>
-      </Paragraph>
-      <Box align="center" my={4}>
+            <p>
+              worked on adding capabilities to an opertaor to automate deployment of
+              services with desired settings
+            </p>
+
+            <br></br>
+
+            <p>
+              containerized applications and services with docker for deployment in kubernetes
+            </p>
+
+          </AccordionPanel>
+
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as='span' flex='1' textAlign='left'>
+                <b>Freelance Python Developer @ Expert Witness Profiler LLC </b>
+                <br></br>
+                <b>(Jun 2022 - Sep 2022)</b>
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+
+          <AccordionPanel pb={4}>
+            <p>
+              wrote a{" "}
+              <NextLink
+                href="https://github.com/akumar23/CourtScraper"
+                passHref
+                scroll={false}
+              >
+                <Link>python script</Link>
+              </NextLink>{" "}
+              that automated data collection from online courts using beautiful soup
+              and selenium and used pandas to make data frames for that webscraped
+              data
+              <br></br>
+              <br></br>
+            </p>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+
+      <br></br>
+
+      <Heading as="h3" variant="section-title">
+        Projects
+      </Heading>
+      <NextLink
+        href="https://github.com/akumar23/digit-predictor-neural-net"
+        passHref
+        scroll={false}
+      >
+        <Link>Digit Predictor Neural Network</Link>
+      </NextLink>
+      <br></br>
+      <NextLink
+        href="https://github.com/akumar23/hf-model-train"
+        passHref
+        scroll={false}
+      >
+        <Link>Huggingface Model Train Script</Link>
+      </NextLink>
+      <br></br>
+      <NextLink
+        href="https://comic-ranker.vercel.app/"
+        passHref
+        scroll={false}
+      >
+        <Link>Comic Ranker Full Stack Webapp</Link>
+      </NextLink>
+
+      <Box align="left" my={4}>
         <NextLink href="/projects" passHref scroll={false}>
-          <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-            Click here to see more of my projects
+          <Button rightIcon={<ChevronRightIcon />} colorScheme="teal" size="sm">
+            more projects
           </Button>
         </NextLink>
       </Box>
     </Section>
 
+    <br></br>
+
     <Section delay={0.1}>
       <Heading as="h3" variant="section-title">
         Skills
       </Heading>
-      <Paragraph>
-        <br></br>
+      <p>
         TypeScript, Python, Java, Go
+        <br></br>
+        neural net and LLM development, vLLM/ollama deployment, huggingface
         <br></br>
         Kubernetes, Docker, Terraform
         <br></br>
@@ -171,7 +216,7 @@ const Home = () => (
         mySQL, Amazon AWS
         <br></br>
         Springboot, Flask, node/next.js
-      </Paragraph>
+      </p>
     </Section>
 
     <Section delay={0.3}>
